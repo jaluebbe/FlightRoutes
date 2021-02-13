d263f82e6cee1673f8905a58e9f3ef24f73e842e
Jannis Luebbe <luebbe@opensky-network.org>
2021-02-14 00:31:34 +0100

    added methods to process and validate OpenSky states.

---
+++ consume_opensky_api.py
@@ -0,0 +1,92 @@
+# encoding=utf8
+import json
+import time
+import arrow
+import redis
+import requests
+import logging
+
+from opensky_api import OpenSkyApi
+from opensky_utils import validated_position
+
+from config import *
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+class OpenSkyApiConsumer:
+
+    last_update = None
+    logger = logging.getLogger(__name__)
+    redis_connection = redis.Redis(host=REDIS_HOST, decode_responses=True)
+
+    def __init__(self):
+        self._initialize_connection()
+
+    def _initialize_connection(self):
+        self.api = OpenSkyApi(OPENSKY_USER, OPENSKY_PASSWORD)
+
+    def worker(self, interval=10):
+        while True:
+            positions = {}
+            t_start = arrow.utcnow()
+            self.logger.info("UTC {}".format(t_start.format("YYYY-MM-DD HH:mm:ss")))
+            try:
+                _response = self.api.get_states()
+                if _response is None:
+                    self.logger.warning("API response is None.")
+                    time.sleep(max((arrow.utcnow() - t_start).total_seconds(), 5))
+                    continue
+                else:
+                    _all_states = _response.states
+                    states_time = _response.time
+            except requests.exceptions.ReadTimeout:
+                self.logger.error("connection timeout.")
+                continue
+            except IOError as e:
+                self.logger.exception("problem with OpenSky Network connection.")
+                self.logger.info("Sleep for 60 seconds.")
+                time.sleep(60)
+                self.logger.info("...reconnecting to OpenSky Network.")
+                self._initialize_connection()
+                continue
+            t_data_received = arrow.utcnow()
+
+            for _state in _all_states:
+                position = validated_position(
+                    _state,
+                    accepted_operators=None,
+                    allow_numerical_callsign=True,
+                    allow_alphanumerical_callsign=True,
+                    allow_on_ground=False,
+                )
+                if position is None:
+                    continue
+                age = states_time - position["time_position"]
+                if age > 15:
+                    continue
+                callsign = position["callsign"]
+                positions[callsign] = position
+            try:
+                self.redis_connection.set(
+                    "opensky_positions",
+                    json.dumps({"positions": positions, "states_time": states_time}),
+                )
+            except (redis.exceptions.TimeoutError, redis.exceptions.ConnectionError):
+                self.logger.exception("Cannot store positions in redis db.")
+            t_data_processed = arrow.utcnow()
+            duration_receiving = (t_data_received - t_start).total_seconds()
+            duration_processing = (t_data_processed - t_data_received).total_seconds()
+            duration_total = (t_data_processed - t_start).total_seconds()
+            self.logger.info(
+                "{} states received ({:.1f}s) and processed ({:.1f}s)."
+                "".format(len(_all_states), duration_receiving, duration_processing)
+            )
+            time.sleep(max(0, interval - duration_total))
+
+
+if __name__ == "__main__":
+
+    osac = OpenSkyApiConsumer()
+    osac.worker()
---
+++ opensky_utils.py
@@ -0,0 +1,70 @@
+import re
+
+# regexp checking for callsign similarity rules
+callsign_validator = re.compile(
+    "^([A-Z]{3})[0-9](([0-9]{0,3})|([0-9]{0,2})([A-Z])|([0-9]?)([A-Z]{2}))$"
+)
+suffix_validator = re.compile("^[1-9]")
+
+# checking if a position from an OpenSky Network state could be used for
+# flight route analysis and if the callsign is formatted as being an airline.
+def validated_position(
+    opensky_state,
+    accepted_operators=None,
+    allow_numerical_callsign=True,
+    allow_alphanumerical_callsign=True,
+    allow_on_ground=False,
+):
+    if opensky_state.callsign is None:
+        return None
+    # Remove trailing whitespaces and convert to upper case.
+    callsign = opensky_state.callsign.strip().upper()
+    # Check if the callsigns fits to Eurocontrol CSS rule ZG00.
+    if callsign_validator.match(callsign) is None:
+        return None
+    # Split callsign into operator and suffix and remove leading zeros.
+    operator = callsign[:3]
+    suffix = callsign[3:].lstrip("0")
+    if suffix_validator.match(suffix) is None:
+        return None
+    callsign = "{}{}".format(operator, suffix)
+    position = {
+        "utc": opensky_state.time_position,
+        "latitude": opensky_state.latitude,
+        "longitude": opensky_state.longitude,
+        "altitude": opensky_state.baro_altitude,
+        "heading": opensky_state.heading,
+        "callsign": callsign,
+        "icao24": opensky_state.icao24,
+        "vertical_rate": opensky_state.vertical_rate,
+        "velocity": opensky_state.velocity,
+        "time_position": opensky_state.time_position,
+        "on_ground": opensky_state.on_ground,
+    }
+    if None in position.values():
+        # incomplete position data or invalid callsign
+        return None
+    if opensky_state.on_ground and not allow_on_ground:
+        # For aircraft on ground, route and schedule verification will
+        # mostly fail.
+        return None
+    flight_level = int(round(opensky_state.baro_altitude / 0.3048 / 100))
+    # maximum FL for Concorde
+    if flight_level > 600:
+        return None
+    position["flight_level"] = flight_level
+    operator = callsign[:3]
+    if accepted_operators is not None and operator not in accepted_operators:
+        return None
+    position["operator_icao"] = operator
+    if opensky_state.sensors is None:
+        position["sensors"] = []
+    else:
+        position["sensors"] = opensky_state.sensors
+    if suffix.isdigit() and allow_numerical_callsign:
+        position["callsign_number"] = int(suffix)
+    elif allow_alphanumerical_callsign:
+        pass
+    else:
+        return None
+    return position
