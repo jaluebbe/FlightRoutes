1f0bbcd4c27a73ad79adecce222b2013411e8777
Jannis Luebbe <luebbe@opensky-network.org>
2023-01-21 21:22:43 +0100

    migrating fmo_data.py , ham_data.py and lux_data.py to use airport_data.py .

--- ham_data.py
+++ ham_data.py
@@ -5,7 +5,7 @@ import pymongo
 import requests
 from airport_info import get_airport_icao
 from airline_info import get_airline_icao
-from route_utils import estimate_max_flight_duration, get_route_length
+import airport_data
 
 from config import HAM_API_KEY
 
@@ -44,11 +44,6 @@ def request_ham_data():
     }
 
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["airports"]
-mycol = mydb["ham"]
-
-
 def _get_date_and_time(flight):
     for _key in ["plannedDepartureTime", "plannedArrivalTime"]:
         if flight.get(_key) is not None:
@@ -60,115 +55,100 @@ def _get_date_and_time(flight):
     return _date, _timestamp.timestamp()
 
 
-def update_ham_data():
-    data = request_ham_data()
-    for _flight in data["arrivals"]:
-        _airline_iata = _flight["airline2LCode"]
-        if _airline_iata is None:
-            continue
-        _flight_number = int(_flight["flightnumber"][3:])
-        _airline_name = _flight["airlineName"]
-        _airline_icao = get_airline_icao(
-            _airline_iata, _airline_name, _flight_number
-        )
-        _date, _timestamp = _get_date_and_time(_flight)
-        _route_items = [get_airport_icao(_flight["originAirport3LCode"])]
-        if _flight["viaAirport3LCode"] is not None:
-            _route_items.append(get_airport_icao(_flight["viaAirport3LCode"]))
-        _route_items.append("EDDH")
-        _route = "-".join(_route_items)
-        _key = "{}_{}_{}_{}".format(
-            _airline_iata, _flight_number, _date, _route
-        )
-        _ham_flight = {
-            "_id": _key,
-            "airline_iata": _airline_iata,
-            "airline_icao": _airline_icao,
-            "airline_name": _airline_name,
-            "flight_number": _flight_number,
-            "arrival": _timestamp,
-            "status": _flight["flightStatusArrival"],
-            "route": _route,
-        }
-        if _flight["flightnumber"] in data["overlapping_flight_numbers"]:
-            _ham_flight["overlap"] = True
-        if _flight["cancelled"]:
-            _ham_flight["cancelled"] = True
-        if _flight["diverted"]:
-            _ham_flight["diverted"] = True
-        try:
-            mycol.insert_one(_ham_flight)
-        except pymongo.errors.DuplicateKeyError:
-            mycol.update_one({"_id": _ham_flight["_id"]}, {"$set": _ham_flight})
-    for _flight in data["departures"]:
-        _airline_iata = _flight["airline2LCode"]
-        if _airline_iata is None:
-            continue
-        _flight_number = int(_flight["flightnumber"][3:])
-        _airline_name = _flight["airlineName"]
-        _airline_icao = get_airline_icao(
-            _airline_iata, _airline_name, _flight_number
-        )
-        _date, _timestamp = _get_date_and_time(_flight)
-        _route_items = ["EDDH"]
-        if _flight["viaAirport3LCode"] is not None:
-            _route_items.append(get_airport_icao(_flight["viaAirport3LCode"]))
-        _route_items.append(
-            get_airport_icao(_flight["destinationAirport3LCode"])
-        )
-        _route = "-".join(_route_items)
-        _key = "{}_{}_{}_{}".format(
-            _airline_iata, _flight_number, _date, _route
-        )
-        _ham_flight = {
-            "_id": _key,
-            "airline_iata": _airline_iata,
-            "airline_icao": _airline_icao,
-            "airline_name": _airline_name,
-            "flight_number": _flight_number,
-            "departure": _timestamp,
-            "status": _flight["flightStatusDeparture"],
-            "route": _route,
-        }
-        if _flight["flightnumber"] in data["overlapping_flight_numbers"]:
-            _ham_flight["overlap"] = True
-        if _flight["cancelled"]:
-            _ham_flight["cancelled"] = True
-        if _flight["diverted"]:
-            _ham_flight["diverted"] = True
-        try:
-            mycol.insert_one(_ham_flight)
-        except pymongo.errors.DuplicateKeyError:
-            mycol.update_one({"_id": _ham_flight["_id"]}, {"$set": _ham_flight})
+class Airport(airport_data.Airport):
+    def __init__(self):
+        super().__init__("ham")
 
-
-def _in_bounds(flight, utc):
-    _departure = flight.get("departure")
-    _arrival = flight.get("arrival")
-    _length = get_route_length(flight["route"])
-    if _departure is not None:
-        return _departure + estimate_max_flight_duration(_length) > utc
-    elif _arrival is not None:
-        return _arrival - estimate_max_flight_duration(_length) < utc
-
-
-def get_active_flights(utc=None):
-    if utc is None:
-        utc = int(arrow.utcnow().timestamp())
-    flights = [
-        _flight
-        for _flight in mycol.find(
-            {
-                "$or": [
-                    {"departure": {"$gt": utc - 24 * 3600, "$lt": utc + 300}},
-                    {"arrival": {"$gt": utc - 300, "$lt": utc + 24 * 3600}},
-                ]
+    def update_data(self):
+        data = request_ham_data()
+        for _flight in data["arrivals"]:
+            _airline_iata = _flight["airline2LCode"]
+            if _airline_iata is None:
+                continue
+            _flight_number = int(_flight["flightnumber"][3:])
+            _airline_name = _flight["airlineName"]
+            _airline_icao = get_airline_icao(
+                _airline_iata, _airline_name, _flight_number
+            )
+            _date, _timestamp = _get_date_and_time(_flight)
+            _route_items = [get_airport_icao(_flight["originAirport3LCode"])]
+            if _flight["viaAirport3LCode"] is not None:
+                _route_items.append(
+                    get_airport_icao(_flight["viaAirport3LCode"])
+                )
+            _route_items.append("EDDH")
+            _route = "-".join(_route_items)
+            _key = "{}_{}_{}_{}".format(
+                _airline_iata, _flight_number, _date, _route
+            )
+            _ham_flight = {
+                "_id": _key,
+                "airline_iata": _airline_iata,
+                "airline_icao": _airline_icao,
+                "airline_name": _airline_name,
+                "flight_number": _flight_number,
+                "arrival": _timestamp,
+                "status": _flight["flightStatusArrival"],
+                "route": _route,
             }
-        )
-        if _in_bounds(_flight, utc)
-    ]
-    return flights
+            if _flight["flightnumber"] in data["overlapping_flight_numbers"]:
+                _ham_flight["overlap"] = True
+            if _flight["cancelled"]:
+                _ham_flight["cancelled"] = True
+            if _flight["diverted"]:
+                _ham_flight["diverted"] = True
+            try:
+                self.mycol.insert_one(_ham_flight)
+            except pymongo.errors.DuplicateKeyError:
+                self.mycol.update_one(
+                    {"_id": _ham_flight["_id"]}, {"$set": _ham_flight}
+                )
+        for _flight in data["departures"]:
+            _airline_iata = _flight["airline2LCode"]
+            if _airline_iata is None:
+                continue
+            _flight_number = int(_flight["flightnumber"][3:])
+            _airline_name = _flight["airlineName"]
+            _airline_icao = get_airline_icao(
+                _airline_iata, _airline_name, _flight_number
+            )
+            _date, _timestamp = _get_date_and_time(_flight)
+            _route_items = ["EDDH"]
+            if _flight["viaAirport3LCode"] is not None:
+                _route_items.append(
+                    get_airport_icao(_flight["viaAirport3LCode"])
+                )
+            _route_items.append(
+                get_airport_icao(_flight["destinationAirport3LCode"])
+            )
+            _route = "-".join(_route_items)
+            _key = "{}_{}_{}_{}".format(
+                _airline_iata, _flight_number, _date, _route
+            )
+            _ham_flight = {
+                "_id": _key,
+                "airline_iata": _airline_iata,
+                "airline_icao": _airline_icao,
+                "airline_name": _airline_name,
+                "flight_number": _flight_number,
+                "departure": _timestamp,
+                "status": _flight["flightStatusDeparture"],
+                "route": _route,
+            }
+            if _flight["flightnumber"] in data["overlapping_flight_numbers"]:
+                _ham_flight["overlap"] = True
+            if _flight["cancelled"]:
+                _ham_flight["cancelled"] = True
+            if _flight["diverted"]:
+                _ham_flight["diverted"] = True
+            try:
+                self.mycol.insert_one(_ham_flight)
+            except pymongo.errors.DuplicateKeyError:
+                self.mycol.update_one(
+                    {"_id": _ham_flight["_id"]}, {"$set": _ham_flight}
+                )
 
 
 if __name__ == "__main__":
-    update_ham_data()
+    airport = Airport()
+    airport.update_data()
