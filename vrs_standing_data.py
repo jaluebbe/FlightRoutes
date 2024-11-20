e3bad2a29f534491f1fbbbabb2c6c0e2303bb08d
Jannis Lübbe <jannis@jl82.de>
2024-11-20 22:36:37 +0100

    integrating with statements for database connection.

--- vrs_standing_data.py
+++ vrs_standing_data.py
@@ -1,60 +1,60 @@
 #!/usr/bin/env python3
-import os
+import pathlib
 import glob
 import csv
 import sqlite3
 from opensky_utils import validated_callsign
 
-PWD = os.path.dirname(os.path.abspath(__file__))
-ROUTES_DB_FILE = f"{PWD}/vrs_routes.sqb"
+PWD = pathlib.Path(__file__).resolve().parent
+ROUTES_DB_FILE = PWD / "vrs_routes.sqb"
 
 
 def refresh_database():
-    db_connection = sqlite3.connect(ROUTES_DB_FILE)
-    _cursor = db_connection.cursor()
-    _cursor.execute(
-        "SELECT count(name) FROM sqlite_master "
-        "WHERE type='table' AND name='flight_routes'"
-    )
+    with sqlite3.connect(ROUTES_DB_FILE) as db_connection:
+        _cursor = db_connection.cursor()
+        _cursor.execute(
+            "SELECT count(name) FROM sqlite_master "
+            "WHERE type='table' AND name='flight_routes'"
+        )
+        if _cursor.fetchone()[0] == 0:
+            with open(PWD / "vrs_routes.sql", encoding="utf-8") as f:
+                db_connection.executescript(f.read())
+        for _file_name in glob.glob(
+            "../standing-data/routes/schema-01/*/*.csv"
+        ):
+            _callsign_info = validated_callsign(_row["Callsign"])
+            if _callsign_info is None:
+                continue
+            with open(_file_name, encoding="utf-8-sig") as csv_file:
+                reader = csv.DictReader(csv_file)
+                for _row in reader:
+                    _cursor.execute(
+                        "REPLACE INTO flight_routes("
+                        "Callsign, OperatorIcao, Route) VALUES(?, ?, ?)",
+                        (
+                            _callsign_info["callsign"],
+                            _row["AirlineCode"],
+                            _row["AirportCodes"],
+                        ),
+                    )
+        _cursor.close()
+        db_connection.commit()
 
-    if _cursor.fetchone()[0] == 0:
-        with open(os.path.join(PWD, "vrs_routes.sql"), encoding="utf-8") as f:
-            db_connection.executescript(f.read())
-    for _file_name in glob.glob("../standing-data/routes/schema-01/*/*.csv"):
-        _callsign = validated_callsign(_row["Callsign"])
-        if _callsign is None:
-            continue
-        with open(_file_name, encoding="utf-8-sig") as csv_file:
-            reader = csv.DictReader(csv_file)
-            for _row in reader:
-                _cursor.execute(
-                    "REPLACE INTO flight_routes(Callsign, OperatorIcao, Route)"
-                    " VALUES(?, ?, ?)",
-                    (
-                        _callsign,
-                        _row["AirlineCode"],
-                        _row["AirportCodes"],
-                    ),
-                )
-    _cursor.close()
-    db_connection.commit()
-    db_connection.execute("VACUUM")
-    db_connection.close()
+    with sqlite3.connect(ROUTES_DB_FILE) as db_connection:
+        db_connection.execute("VACUUM")
 
 
-def get_flight_route(callsign: str) -> dict:
-    connection = sqlite3.connect(ROUTES_DB_FILE)
-    _cursor = connection.cursor()
-    _cursor.execute(
-        "SELECT Route from flight_routes WHERE Callsign=?",
-        (callsign,),
-    )
-    result = _cursor.fetchone()
-    _cursor.close()
-    connection.close()
-    if result is None:
-        return None
-    return result[0]
+def get_flight_route(callsign: str) -> dict | None:
+    with sqlite3.connect(ROUTES_DB_FILE) as connection:
+        _cursor = connection.cursor()
+        _cursor.execute(
+            "SELECT Route from flight_routes WHERE Callsign=?",
+            (callsign,),
+        )
+        result = _cursor.fetchone()
+        _cursor.close()
+    if result is not None:
+        return result[0]
 
 
 if __name__ == "__main__":
