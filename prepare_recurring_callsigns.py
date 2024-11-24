d6e171f563547f1dd28b8141cba1f12d032dcc3e
Jannis Lübbe <luebbe@opensky-network.org>
2024-11-24 17:25:03 +0100

    added prepare_recurring_callsigns.py in a version that works with trino.

---
+++ prepare_recurring_callsigns.py
@@ -0,0 +1,70 @@
+import json
+import pandas as pd
+import arrow
+from pyopensky.trino import Trino
+
+trino = Trino()
+
+end_date = arrow.utcnow().shift(hours=-1).floor("day")
+start_date = end_date.shift(days=-22)
+
+
+def process_flightlist(flightlist: pd.DataFrame) -> pd.DataFrame:
+    flightlist["callsign"] = flightlist["callsign"].str.rstrip()
+    callsign_components = flightlist["callsign"].str.extract(
+        r"^(?P<operator>[A-Z]{3})0*(?P<suffix>[1-9][A-Z0-9]*)$", expand=True
+    )
+    flightlist["callsign"] = (
+        callsign_components["operator"] + callsign_components["suffix"]
+    )
+    flightlist.dropna(subset=["callsign"], inplace=True)
+    flightlist = flightlist[
+        flightlist["callsign"].str.contains(
+            r"^(?:[A-Z]{3})[1-9](?:(?:[0-9]{0,3})|(?:[0-9]{0,2})"
+            r"(?:[A-Z])|(?:[0-9]?)(?:[A-Z]{2}))$",
+            regex=True,
+            na=False,
+        )
+    ].copy()
+    flightlist["lastseen"] = flightlist["lastseen"].astype(int) // 10**9
+    flightlist["firstseen"] = flightlist["firstseen"].astype(int) // 10**9
+    return flightlist
+
+
+_before = arrow.utcnow()
+
+dataframes = []
+current_date = start_date
+while current_date < end_date:
+    _flightlist = trino.flightlist(current_date.format("YYYY-MM-DD"))
+    dataframes.append(process_flightlist(_flightlist))
+    current_date = current_date.shift(days=1)
+callsign_occurences = pd.concat(dataframes, ignore_index=True)
+
+_after = arrow.utcnow()
+duration = (_after - _before).total_seconds()
+
+callsign_occurences = callsign_occurences.groupby(["callsign"]).agg(
+    {"lastseen": "max", "firstseen": "min"}
+)
+recurring_callsigns = callsign_occurences[
+    callsign_occurences.lastseen - callsign_occurences.firstseen > 86400
+]
+
+_json_data = json.dumps(
+    {
+        "start_date": start_date.timestamp(),
+        "end_date": end_date.timestamp(),
+        "recurring_callsigns": recurring_callsigns.index.to_list(),
+    }
+)
+
+with open("recurring_callsigns.json", "w") as f:
+    f.write(_json_data + "\n")
+
+print(
+    f"Found {len(recurring_callsigns)} recurring callsigns out of "
+    f"{len(callsign_occurences)} different callsigns in the time range from "
+    f"{start_date.format('YYYY-MM-DD HH:mm:ss')} to "
+    f"{end_date.format('YYYY-MM-DD HH:mm:ss')} within {duration:.1f}s."
+)
